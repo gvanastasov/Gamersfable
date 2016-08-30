0fe582a7c1854ea122463ae35455eb76f6722cf7
gva <gvanastasov@abv.bg>
2016-08-30 21:54:31 +0200

    moved the category from the stories VC to games VC, small tweaks on the their views -> proper linking actions

--- Gamersfable_prototype/Gamersfable_prototype/Controllers/GamesController.cs
+++ Gamersfable_prototype/Gamersfable_prototype/Controllers/GamesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Gamersfable_prototype.Models;
+using System.Net;
 
 namespace Gamersfable_prototype.Controllers
 {
@@ -15,6 +16,22 @@ namespace Gamersfable_prototype.Controllers
         public ActionResult Index()
         {
             return View(db.Games.ToList());
+        }
+
+        // GET: Games/Category/
+        public ActionResult Category(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var stories = db.Stories.Where(s => s.Game_Id == id);
+            if (stories == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StoriesGameTitle = db.Games.FirstOrDefault(g => g.Id == id).Title;
+            return View(stories.ToList());
         }
     }
 }
